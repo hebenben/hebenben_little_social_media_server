f3cc282b4580dfe50c5d9e4eb8733086f63e0e37
hebenben <heyiming11@gmail.com>
2022-09-24 02:23:13 -0400

    cors update

--- index.js
+++ index.js
@@ -29,6 +29,21 @@ app.use(cors());
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.setHeader('Cross-origin-Embedder-Policy', 'require-corp');
+    res.setHeader('Cross-origin-Opener-Policy','same-origin');
+  
+    if (req.method === 'OPTIONS') {
+      res.sendStatus(200)
+    } else {
+      next()
+    }
+  });
+
+  
 
 const storage = multer.diskStorage({
     destination:(req,file,cb)=>{
@@ -38,6 +53,7 @@ const storage = multer.diskStorage({
         cb(null, req.body.name);
     }
 })
+
 
 const upload = multer({storage});
 app.post("/api/upload",upload.single("file"),(req,res) => {
