a74c5b4ce686074550ad698a5f1f3f5d4a767ed4
hebenben <heyiming11@gmail.com>
2022-09-24 11:14:41 -0400

    cors setting update

--- index.js
+++ index.js
@@ -29,12 +29,12 @@ app.use(cors());
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    res.setHeader('Cross-origin-Embedder-Policy', 'require-corp');
-    res.setHeader('Cross-origin-Opener-Policy','same-origin');
+app.all('*', function(req, res, next){
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.header('Cross-origin-Embedder-Policy', 'require-corp');
+    res.header('Cross-origin-Opener-Policy','same-origin');
   
     if (req.method === 'OPTIONS') {
       res.sendStatus(200)
