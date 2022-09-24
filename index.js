d6be475325cb7486b96ac36c33b64122e732c7c0
hebenben <heyiming11@gmail.com>
2022-09-24 11:57:45 -0400

    cros update

--- index.js
+++ index.js
@@ -33,8 +33,6 @@ app.all('*', function(req, res, next){
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    res.header('Cross-origin-Embedder-Policy', 'require-corp');
-    res.header('Cross-origin-Opener-Policy','same-origin');
   
     if (req.method === 'OPTIONS') {
       res.sendStatus(200)
